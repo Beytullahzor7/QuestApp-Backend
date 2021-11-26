ddec16e3cfea63ce20ad97c9d3581b1d4c772f36
Beytullahzor7 <beytullahzor7@gmail.com>
2021-11-26 17:33:21 +0300

    Backend Created

---
+++ questapp/src/main/java/com/project/questapp/entities/Like.java
@@ -0,0 +1,21 @@
+package com.project.questapp.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name="p_like")
+@Data
+public class Like {
+	
+	@Id
+	Long id;
+	
+	Long postId;
+	Long userId;
+	
+
+}

218b23f80846251592e998ff406d254a7eb8ece8
Beytullahzor7 <beytullahzor7@gmail.com>
2021-12-05 00:27:17 +0300

    Like Response Added

--- questapp/src/main/java/com/project/questapp/entities/Comment.java
+++ questapp/src/main/java/com/project/questapp/entities/Comment.java
@@ -4,6 +4,8 @@ import javax.persistence.Column;
 
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
@@ -23,6 +25,7 @@ import lombok.Data;
 public class Comment {
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	Long id;
 	
 	@ManyToOne(fetch = FetchType.LAZY)
--- questapp/src/main/java/com/project/questapp/responses/PostResponse.java
+++ questapp/src/main/java/com/project/questapp/responses/PostResponse.java
@@ -1,5 +1,7 @@
 package com.project.questapp.responses;
 
+import java.util.List;
+
 import com.project.questapp.entities.Post;
 
 import lombok.Data;
@@ -12,12 +14,14 @@ public class PostResponse {
 	String userName;
 	String title;
 	String text;
+	List<LikeResponse> postLikes;
 	
-	public PostResponse(Post entity) {
+	public PostResponse(Post entity, List<LikeResponse> likes) {
 		this.id = entity.getId();
 		this.userId = entity.getUser().getId();
 		this.userName = entity.getUser().getUserName();
 		this.title = entity.getTitle();
 		this.text = entity.getText();
+		this.postLikes = likes;
 	}
 }
